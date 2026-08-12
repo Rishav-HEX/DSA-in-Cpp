fd6de6f6e10a7abd06743d7daa34a087583489c5
Rishav-HEX <guptarishav4321@gmail.com>
2026-08-13 00:56:08 +0530

    All Possible Subarrays Printing Using Brute Force

--- Arrays/KadaneAlgotithm.cpp
+++ Arrays/KadaneAlgotithm.cpp
@@ -1,4 +1,43 @@
+/** What is Kadane's Algorithm?
+ * Kadane's Algorithm is an efficient method to find the maximum sum of a contiguous subarray within a one-dimensional array of numbers. It operates in linear time O(n) and uses a dynamic programming approach to keep track of the maximum sum found so far and the current sum of the subarray being considered. The algorithm iterates through the array, updating the current sum and the maximum sum as it goes, ensuring that it captures the largest possible sum of any contiguous subarray.
+ */
+// Code for Printing all the possible Subarrays of an Array
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n =5 ;
+    int arr[] = {1 ,2 ,3,4,5} ;
+
+    for (int st = 0; st < n; st++){
+        for (int end = st; end < n; end++){
+                  cout << "[" ;
+            for(int i = st; i <= end; i++)
+            {
+              cout << arr[i] << "";
+            } cout << "]" << endl ;           
+        }
+    }
+return 0;    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Implentation of Kadane's Algorithm to find the maximum sum of a contiguous subarray in an array of integers using Brute Force Approach
+/*
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -19,4 +58,4 @@ int main()
     cout << "Maximum sum of contiguous subarray is: " << max_sum << endl;
 
     return 0;
-}
+}*/
